21ed6d8e741d18212eb187b78d09030a0512a0fc
Ngân Khánh Nguyễn Thị <ngankhanh98@gmail.com>
2020-03-18 09:00:46 +0700

    using json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db/tkb.json");
+const db = low(adapter);
+var obj = db
+  .get("tkb")
+  .value();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render('index', { row: obj });
+  console.log(obj);
 });
 
 module.exports = router;
